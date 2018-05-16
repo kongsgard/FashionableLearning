8acaa4ea0406085be325a594d1adf7aa302bd2ff
Morten Lie <morli@stud.ntnu.no>
2018-05-16 12:16:01 -0700

    Add a VGG module

--- main/main.py
+++ main/main.py
@@ -1,41 +1,48 @@
 import tensorflow as tf
+import multiprocessing
 
 import _init_paths
 from data_loader.fashion_mnist_data import DataGenerator
-from models.softmax_model import SoftmaxModel
-from trainers.softmax_trainer import SoftmaxTrainer
-from utils.config import process_config
+from utils.config import process_config, import_class
 from utils.dirs import create_dirs
 from utils.logger import Logger
 from utils.utils import get_args
 
 
 def main():
+    # Capture the config path from the run arguments
+    # then process the json configuration file
     try:
         args = get_args()
         config = process_config(args.config)
+
+        Model = import_class(args.config, "model")
+        Trainer = import_class(args.config, "trainer")
     except:
         print("Missing or invalid arguments")
         exit(0)
 
     # Create the experiments dirs
     create_dirs([config.summary_dir, config.checkpoint_dir])
-    # Create tensorflow session
-    sess = tf.Session()
-    # Create an instance of the model you want
-    model = SoftmaxModel(config)
-    # Load model if exists
+    # Create a tensorflow session
+    sess = tf.InteractiveSession()
+    # Create an instance of the model specified
+    model = Model(config)
+    # Load model if it exists
     model.load(sess)
-    # Create your data generator
+    # Create the data generator
     data = DataGenerator(config)
     # Create tensorboard logger
     logger = Logger(sess, config)
     # Create trainer and pass all the previous components to it
-    trainer = SoftmaxTrainer(sess, model, data, config, logger)
+    trainer = Trainer(sess, model, data, config, logger)
 
     # Train the model
     trainer.train()
-
+    print("Training complete!")
+    sess.close()
 
 if __name__ == '__main__':
-    main()
+    p = multiprocessing.Process(target=main)
+    p.start()
+    p.join()
