ed33f7809db72af4066b1fdc45c8caa2d958c97e
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2018-05-21 12:01:32 -0700

    Add support for estimating test data loss and accuracy

--- data_loader/fashion_mnist_data.py
+++ data_loader/fashion_mnist_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from tensorflow.examples.tutorials.mnist import input_data
 
 fashion_mnist = input_data.read_data_sets('data/fashion', source_url='http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/', one_hot=True)
@@ -7,11 +8,8 @@ class DataGenerator:
     def __init__(self, config):
         self.config = config
 
-        # Import data
-        self.input = fashion_mnist.test.images
-        self.y = fashion_mnist.test.labels
-
     def next_batch(self, batch_size):
         yield fashion_mnist.train.next_batch(self.config.batch_size)
-        #idx = np.random.choice(500, batch_size)
-        #yield self.input[idx], self.y[idx]
+
+    def get_test_data(self):
+        return fashion_mnist.test.images, fashion_mnist.test.labels
