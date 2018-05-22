e51527e264b7d7f211d97f478be693430b5897eb
lie930 <morli@stud.ntnu.no>
2018-05-21 20:10:35 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -8,6 +8,7 @@ from utils.dirs import create_dirs
 from utils.logger import Logger
 from utils.utils import get_args
 
+
 def main():
     # Capture the config path from the run arguments
     # then process the json configuration file
