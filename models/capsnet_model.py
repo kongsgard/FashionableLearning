62d7943122546bb0797a3ae56f65ba26d8203e94
Morten Lie <morli@stud.ntnu.no>
2018-05-23 14:41:05 -0700

    Added capsnet files

---
+++ models/capsnet_model.py
@@ -0,0 +1,54 @@
+from base.base_model import BaseModel
+import tensorflow as tf
+
+
+class CapsnetModel(BaseModel):
+    def __init__(self, config):
+        super(SimpleCNNModel, self).__init__(config)
+        self.build_model()
+        self.init_saver()
+
+    def build_model(self):
+
+
+
+
+
+
+
+
+
+
+
+    x = layers.Input(shape=input_shape)
+
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, num_routing=num_routing,
+                             name='digitcaps')(primarycaps)
+
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    # If using tensorflow, this will not be necessary. :)
+    out_caps = Length(name='capsnet')(digitcaps)
+
+    # Decoder network.
+    y = layers.Input(shape=(n_class,))
+    masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
+    masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
+
+    # Shared Decoder model in training and prediction
+    decoder = models.Sequential(name='decoder')
+    decoder.add(layers.Dense(512, activation='relu', input_dim=16*n_class))
+    decoder.add(layers.Dense(1024, activation='relu'))
+    decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
+    decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
+
+    # Models for training and evaluation (prediction)
+    train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
+    eval_model = models.Model(x, [out_caps, decoder(masked)])
+    return train_model, eval_model
