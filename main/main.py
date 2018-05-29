9fc10578f423b86b9f54042d64e32a4f926574f5
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2018-05-29 16:35:14 -0700

    Add batch normalization

--- main/main.py
+++ main/main.py
@@ -8,6 +8,7 @@ from utils.dirs import create_dirs
 from utils.logger import Logger
 from utils.utils import get_args
 
+from models.CNN_one_conv_layer_model import CNNOneConvLayerModel
 
 def main():
     # Capture the config path from the run arguments
